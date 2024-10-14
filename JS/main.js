b02cbcb10601924f2df7c48c3568383c05ee7973
Nàt <82668500+4D4J@users.noreply.github.com>
2024-10-14 22:53:57 +0200

    add des images + test des premières cartes d'intéraction

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.slider-img');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        if (i === index) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
